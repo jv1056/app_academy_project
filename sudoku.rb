2c9c5809827f558952c5d4886a3cbe51e6ac5341
App Academy Student <kjjafshf>
2018-01-18 15:07:04 -0500

    Add render

--- sudoku.rb
+++ sudoku.rb
@@ -15,21 +15,21 @@ class SudokuGame
   end
 
   def retrieve_pos_from_ui
-    p = nil
-    until p && legal_illegibility_of_p?(p)
+    pos = nil
+    until pos && legal_illegibility_of_p?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
 
       begin
-        p = parse_inanity(gets.chomp)
+        pos = parse_inanity(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
-        p = nil
+        pos = nil
       end
     end
-    p
+    pos
   end
 
   def retrieve_value_from_ui
@@ -51,11 +51,12 @@ class SudokuGame
   end
 
   def process_parameters
+    board.render
     pos_to_val(retrieve_pos_from_ui, retrieve_value_from_ui)
   end
 
-  def pos_to_val(p, v)
-    board[p] = v
+  def pos_to_val(pos, v)
+    board[pos] = v
   end
 
   def commence_proceedings
